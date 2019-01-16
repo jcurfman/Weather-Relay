6a9799ae01281102d8d8029b4cf4b844aa5a08bd
Joshua Curfman <joshua.curfman@tecnovelty.com>
2019-01-16 13:40:24 -0800

    CPU ID generation, data inclusion
    A function has been included to generate the SAMD processor identifier onboard the Adafruit Feathers used for this project and return the last portion of it for use. Additionally, the reply message now includes data instead of being a dummy string- currently, the CPU ID value is used.

--- Feather_LoRa_RX_test.ino
+++ Feather_LoRa_RX_test.ino
@@ -18,6 +18,10 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 //blinky for receipt of message- useful to headless range testing
 #define LED 13
 
+String idNum;
+volatile uint32_t blah;
+char message[50];
+
 void setup() {
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
@@ -52,6 +56,9 @@ void setup() {
 
   //transmitter power? Tinker.
   rf95.setTxPower(23, false);
+
+  //Get identity
+  chipId();
 }
 
 void loop() {
@@ -67,12 +74,15 @@ void loop() {
       Serial.print("RSSI: ");
       Serial.println(rf95.lastRssi(), DEC);
 
+      //Generate message to send incorporating data- here, the CPU Identifier
+      sprintf(message, "Hello, my ID is: %8x", blah);
+
       //Send reply
-      uint8_t data[] = "And hello back to you";
-      rf95.send(data, sizeof(data));
+      rf95.send((uint8_t *) message, sizeof(message));
       rf95.waitPacketSent();
       Serial.println("Sent a reply");
       digitalWrite(LED, LOW);
+      
     }
     else {
       Serial.println("Receive failed");
@@ -80,5 +90,45 @@ void loop() {
   }
   else {
     digitalWrite(LED, LOW);
+    Serial.println("No message recieved");
   }
+
+  //The following block is for testing purposes only
+  sprintf(message, "Hello, my ID is: %8x", blah);
+  Serial.println(message);
 }
+
+/**
+void sendReply(uint8_t data) {
+  //Transmits "value" back over the network
+  //uint8_t data[] = value;
+  rf95.send(data, sizeof(data));
+  rf95.waitPacketSent();
+  Serial.println("Sent a reply");
+  digitalWrite(LED, LOW);
+}
+*/
+
+void chipId() {
+  //Assembles the SAMD processor chip ID value
+  volatile uint32_t val1, val2, val3, val4;
+  volatile uint32_t *ptr1 = (volatile uint32_t *)0x0080A00C;
+  val1 = *ptr1;
+  volatile uint32_t *ptr = (volatile uint32_t *)0x0080A040;
+  val2 = *ptr;
+  ptr++;
+  val3 = *ptr;
+  ptr++;
+  val4 = *ptr;
+  
+  //Prints the chip ID if following block is uncommented.
+  /** Serial.print("chip id: 0x");
+  char buf[33];
+  sprintf(buf, "%8x%8x%8x%8x", val1, val2, val3, val4);
+  Serial.println(buf); */
+  char buf2[12];
+  sprintf(buf2, "%8x", val4);
+  idNum = buf2;
+  blah = val4;
+}
+
